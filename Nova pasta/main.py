7f6f2d61ff8e88d4ca3c0ef6d8f7f6c9533fbd33
Natanael <natannakenoael@gmail.com>
2025-03-19 21:16:36 -0300

    Add files via upload

--- Nova pasta/main.py
+++ Nova pasta/main.py
@@ -1,14 +1,31 @@
 import pygame # type: ignore
+import random
 import math
 
 class snake:
     def __init__(self) -> None:
-        pass
+        self.head = segment(15, 15, (255, 255, 255), (100, 100))
+        self.body = [self.head]
+        self.addSegment()
+        self.addSegment()
+        self.addSegment()
+
+    def draw(self, screen):
+         for s in self.body:
+            s.draw(screen)
+
+    def updateSegments(self):
+        for s in self.body:
+            s.update()
+
+    def addSegment(self):
+        self.body.append(segment(10, 10, (255, 255, 127), (0, 0), (self.body[-1], 10)))
 
 class segment:
     def __init__(self, width, height, color, pos, fixed = (None, None)) -> None:
         self.img = pygame.Surface((width, height))
         self.img.fill(color)
+        self.rect = self.img.get_rect(center=pos)
         self.pos = list(pos)
         self.fixed = fixed
 
@@ -16,11 +33,15 @@ class segment:
         screen.blit(self.img, self.pos)
 
     def update(self) -> None:
+        self.rect.center = self.pos
         if self.fixed[0] is not None:
             angle = math.atan2(self.fixed[0].pos[1] - self.pos[1], self.fixed[0].pos[0] - self.pos[0])
 
             self.pos[0] = self.fixed[0].pos[0] - math.cos(angle) * self.fixed[1]
             self.pos[1] = self.fixed[0].pos[1] - math.sin(angle) * self.fixed[1]
+
+    def collision(self):
+        pass
 
 class Windows:
     def __init__(self, width, height) -> None:
@@ -32,14 +53,18 @@ class Windows:
     def Render(self) -> None:
         self.screen.fill((0, 0, 0))
 
-        for s in body:
-            s.draw(self.screen)
+        sn.draw(self.screen)
+        apple.draw(self.screen)
 
         pygame.display.flip()
 
     def Update(self) -> None:
-        for s in body:
-            s.update()
+        sn.updateSegments()
+
+        if apple.rect.colliderect(sn.head.rect):
+            sn.addSegment()
+            apple.pos = (random.randint(1, 799), random.randint(1, 699))
+            apple.update()
 
     def HandleProc(self) -> None:
         for event in pygame.event.get():
@@ -48,8 +73,22 @@ class Windows:
         
         keys = pygame.key.get_pressed()
 
-        sA.pos[1] += (keys[pygame.K_s] - keys[pygame.K_w]) * 3
-        sA.pos[0] += (keys[pygame.K_d] - keys[pygame.K_a]) * 3
+        if sn.head.rect.top >= 0 and sn.head.rect.bottom =< 600 and sn.head.rect.left >= 0 and sn.head.rect.right =< 800:
+            sn.head.pos[1] += (keys[pygame.K_s] - keys[pygame.K_w]) * 3
+            sn.head.pos[0] += (keys[pygame.K_d] - keys[pygame.K_a]) * 3
+        else:
+            if sn.head.rect.top < 300:
+                sn.head.pos[1] = sn.head.pos[1] + 1
+            else:
+                sn.head.pos[1] = sn.head.pos[1] - 1 
+
+            if sn.head.rect.left < 300:
+                sn.head.pos[0] = sn.head.pos[0] + 1
+            else:
+                sn.head.pos[0] = sn.head.pos[0] - 1
+
+        if keys[pygame.K_p]:
+            sn.addSegment()
 
     def Run(self) -> None:
         while self.running:
@@ -61,11 +100,7 @@ class Windows:
 
 win = Windows(800, 600)
 
-sA = segment(10, 10, (255, 255, 255), (100, 100))
-sB = segment(10, 10, (255, 255, 127), (0, 0), (sA, 10))
-sC = segment(10, 10, (255, 255, 127), (0, 0), (sB, 10))
-sD = segment(10, 10, (255, 255, 127), (0, 0), (sC, 10))
-sE = segment(10, 10, (255, 255, 127), (0, 0), (sD, 10))
-body = [sA, sB, sC, sD, sE]
+sn = snake()
+apple = segment(10, 10, (255, 61, 0), (random.randint(0, 800), random.randint(0, 600)))
 
 win.Run()
